f4965560a1110da2a7e99ee22d334aabd4adc1c7
Pibarel Maisnam <maisnamakashsingh@gmail.com>
2024-12-21 08:16:11 +0530

    Resume

---
+++ src/components/Education/Education.js
@@ -0,0 +1,190 @@
+
+const degrees = {
+    degrees: [
+      {
+        title: "Manipur University",
+        subtitle: "Masters in History",
+        logo_path: "MTU.png",
+        alt_name: "MTU",
+        duration: "2020 - 2024",
+        descriptions: [
+          "⚡ Studied History of Manipur from early times to 1467 , Social History of Modern India, etc",
+          "⚡ Learn World History, 1775-1919, Historiography",
+          "⚡Dissertation",
+        ],
+        website_link:
+          "https://www.manipuruniv.ac.in/department/history-department",
+      },
+      {
+        title: "Kamakhyalal Bajoria College",
+        subtitle: "B.A. History Honours ",
+        logo_path: "JHSS.png",
+        alt_name: "KL",
+        duration: "2019 - 2022",
+        descriptions: [
+          "⚡ I have studied  subjects like Sociology, Political Science, Education, etc.",
+          "⚡ I have also completed various online courses ",
+        ],
+        website_link: "https://www.klbajoriacollege.org/index.html ",
+      },
+      {
+        title: "Johsntone Higher Secondary School",
+        subtitle: "Humanities",
+        logo_path: "logo",
+        alt_name: "jhss",
+        duration: "2017 - 2019",
+        descriptions: [
+          "⚡ I have studied  subjects like Sociology, Political Science, Education, etc.",
+        ],
+        website_link:
+          "https://en.wikipedia.org/wiki/Johnstone_Higher_Secondary_School,_Imphal ",
+      },
+      {
+        title: "St. joseph's Higher secondary School",
+        subtitle: "High School",
+        logo_path: "sjs.png",
+        alt_name: "jhss",
+        duration: "2013 - 2017",
+        descriptions: [
+          "⚡ I have studied  subjects like Science, Social Science, Mathematics, etc.",
+        ],
+        website_link: "https://sjschoolimphal.edu.in/",
+      },
+    ],
+  };
+  
+  const experience = {
+    title: "Experience",
+    subtitle: "Work, Internship and Volunteership",
+    description:
+      "I've completed two internships. I've mostly done projects on my own and I am actively looking for internships. I love organizing workshops to share my knowledge with others.",
+    header_image_path: "experience.svg",
+    sections: [
+      {
+        title: "Work",
+        experiences: [
+          {
+            title: "IT Developer",
+            company: "Canada Revenue Agency",
+            company_url: "https://www.canada.ca/en/revenue-agency.html",
+            logo_path: "cra.png",
+            duration: "May 2023 - Ongoing",
+            location: "Halifax, Nova Scotia",
+            description:
+              "Canada Revenue Agency (CRA) administers tax laws for the Government of Canada and for most provinces and territories, and administers various social and economic benefit and incentive programs delivered through the tax system.",
+            color: "#f10000",
+          },
+          {
+            title: "Full Stack Developer",
+            company: "Quicko",
+            company_url: "https://quicko.com/",
+            logo_path: "quicko.jpg",
+            duration: "June 2021 - October 2021",
+            location: "Ahmedabad",
+            description:
+              "Quicko is an online tax planning, preparation & filing platform for individuals & businesses.",
+            color: "#2962FF",
+          },
+        ],
+      },
+      {
+        title: "Internships",
+        experiences: [
+          {
+            title: "Google Summer of Code",
+            company: "Sugar Labs",
+            company_url: "https://sugarlabs.org/",
+            logo_path: "google_logo.png",
+            duration: "May 2019 - Aug 2019",
+            location: "Work From Home",
+            description:
+              "I worked on the Dashboard project which helps users track their activities while using Sugar OS. I also worked on making a Tamagotchi-like widget for Sugar's Home Screen",
+            color: "#ee3c26",
+          },
+          {
+            title: "Android App Developer Intern",
+            company: "IAS4Sure",
+            company_url: " ",
+            logo_path: "buld.jpg",
+            duration: "December 2019 - February 2020",
+            location: "Work From Home",
+            description:
+              "Internship task was to make a native Android application. ",
+            color: "#0071C5",
+          },
+          {
+            title: "Web Developer",
+            company: "Wrighter Writing Solutions",
+            company_url: " ",
+            logo_path: "wrighter.jpg",
+            duration: "August 2020",
+            location: "",
+            description: "Develop a website using PHP and jQuery.",
+            color: "#56A4D3",
+          },
+          {
+            title: "Front-End Developer",
+            company: "VJ TechServe",
+            company_url: " ",
+            logo_path: "vjt.png",
+            duration: "September 2020",
+            location: "",
+            description: "Develop a portfolio website using ReactJS",
+            color: "#800000",
+          },
+        ],
+      },
+      {
+        title: "Volunteerships",
+        experiences: [
+          {
+            title: "Google Summer of Code Mentor",
+            company: "Sugar Labs / GSoC",
+            company_url: "https://sugarlabs.org/",
+            logo_path: "google_logo.png",
+            duration: "Summer of 2017 / 18 / 20",
+            location: " ",
+            description:
+              "Mentorship responsibilities were to help students plan the project, review issues and pull requests, ensure smooth progress and help them out when they are stuck.",
+            color: "#4285F4",
+          },
+          {
+            title: "Google Code-In Mentor",
+            company: "Sugar Labs / GSoC",
+            company_url: "https://sugarlabs.org/",
+            logo_path: "google_logo.png",
+            duration: "Winter of 2017 / 18 / 19",
+            location: " ",
+            description:
+              "Day to day resposibilites of this mentorship was to help out children aged 13-17 get started with programming and open-source, review their work and approve them.",
+            color: "#D83B01",
+          },
+          {
+            title: "Board Member at Codeuino",
+            company: " ",
+            company_url: " ",
+            logo_path: "codeuino.jpg",
+            duration: "2018 - 2019",
+            location: " ",
+            description: "Previous Board during early days of Codeuino",
+            color: "#D83B01",
+          },
+          {
+            title: "BluWings Club",
+            company: " ",
+            company_url: " ",
+            logo_path: "b.png",
+            duration: "2018 - On Going",
+            location: " ",
+            description:
+              "Co-Founder and President of the programming club called BluWings. Have organized several workshops which introduced programming and related tools to freshers.",
+            color: "#D83B01",
+          },
+        ],
+      },
+    ],
+  };
+  export {
+    degrees,
+    experience,
+  };
